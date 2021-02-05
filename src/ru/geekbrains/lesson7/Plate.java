b08d98e466c58dce52e71ffaed0e7704fadf61a9
Sergey Skidanov <vonadiks@mail.ru>
2021-02-05 23:14:53 +0400

    Cat & Plate without changes

---
+++ src/ru/geekbrains/lesson7/Plate.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.lesson7;
+
+public class Plate
+{
+    private int food;
+    public Plate(int food) {
+        this.food = food;
+    }
+    public void decreaseFood(int n) {
+        food -= n;
+    }
+    public void info() {
+        System.out.println("plate: " + food);
+    }
+
+}
